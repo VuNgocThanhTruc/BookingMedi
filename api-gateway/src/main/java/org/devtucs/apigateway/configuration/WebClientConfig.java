a597769f7de1a1f0e8d40ef675d4bc1876f5804a
VuNgocThanhTruc <vutruc0702@gmail.com>
2024-10-04 16:41:22 +0700

    [Truc/backend]: Configuration cors

--- api-gateway/src/main/java/org/devtucs/apigateway/configuration/WebClientConfig.java
+++ api-gateway/src/main/java/org/devtucs/apigateway/configuration/WebClientConfig.java
@@ -3,9 +3,14 @@ package org.devtucs.apigateway.configuration;
 import org.devtucs.apigateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @Configuration
 public class WebClientConfig {
@@ -14,6 +19,18 @@ public class WebClientConfig {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/identity")
                 .build();
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 
     @Bean
